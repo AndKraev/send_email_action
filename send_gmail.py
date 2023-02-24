f4a0e959bf9fe022edcfd8dabf693b6009a464f8
Краев Андрей Дмитриевич <kraev.a@vtb.ru>
2023-02-24 12:37:25 +0300

    init

---
+++ send_gmail.py
@@ -0,0 +1,34 @@
+import os
+import smtplib
+
+
+def send_email(
+        user: str, pwd: str, recipient: [str | list[str]], subject: str, body: str
+) -> None:
+
+    from_ = user
+    to = recipient if isinstance(recipient, list) else [recipient]
+    subject = subject
+    text = body
+
+    # Prepare actual message
+    message = """From: %s\nTo: %s\nSubject: %s\n\n%s
+    """ % (from_, ", ".join(to), subject, text)
+
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.ehlo()
+    server.starttls()
+    server.login(user, pwd)
+    server.sendmail(from_, to, message)
+    server.close()
+    print('Email has been successfully sent.')
+
+
+if __name__ == "__main__":
+    send_email(
+        user=os.getenv("USERNAME"),
+        pwd=os.getenv("PASSWORD"),
+        recipient=os.getenv("RECIPIENT"),
+        subject=os.getenv("SUBJECT"),
+        body=os.getenv("BODY")
+    )
